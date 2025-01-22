18f56c5a6824d508b90d9af0707c11e2a8b5430b
Peterson <pettm20@gmail.com>
2025-01-22 14:31:14 -0300

    get lists

---
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
+
+
+//JPA repository é um componete que faz todas as operações 
